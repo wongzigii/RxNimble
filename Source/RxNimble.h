97cd27365c01896df8874bb75a71beeec00b7b6d
Zigii Wong <wongzigii@gmail.com>
2018-05-22 12:34:57 +0800

    Carthage support

---
+++ Source/RxNimble.h
@@ -0,0 +1,18 @@
+//
+//  RxNimble.h
+//  RxNimble
+//
+//  Created by Wong Zigii on 2018/5/22.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxNimble.
+FOUNDATION_EXPORT double RxNimbleVersionNumber;
+
+//! Project version string for RxNimble.
+FOUNDATION_EXPORT const unsigned char RxNimbleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxNimble/PublicHeader.h>
+
+
